aad426a1ff0153d0a8072b4f09200b0b50b93768
Pavel Rojtberg <rojtberg@gmail.com>
2020-10-17 01:01:02 +0200

    DROP THIS: turn imgui sample into particle benchmark

--- Samples/Simple/include/ImGuiDemo.h
+++ Samples/Simple/include/ImGuiDemo.h
@@ -21,59 +21,25 @@ public:
         mInfo["Thumbnail"] = "thumb_imgui.png";
     }
 
-    void preViewportUpdate(const RenderTargetViewportEvent& evt)
-    {
-        if(!evt.source->getOverlaysEnabled()) return;
-        if(!mTrayMgr->getTraysLayer()->isVisible()) return;
-
-        ImGuiOverlay::NewFrame();
-
-        ImGui::ShowDemoWindow();
-    }
-
-    bool keyPressed(const KeyboardEvent& evt) { return mListenerChain.keyPressed(evt); }
-    bool keyReleased(const KeyboardEvent& evt) { return mListenerChain.keyReleased(evt); }
-    bool mouseMoved(const MouseMotionEvent& evt) { return mListenerChain.mouseMoved(evt); }
-    bool mouseWheelRolled(const MouseWheelEvent& evt) { return mListenerChain.mouseWheelRolled(evt); }
-    bool mousePressed(const MouseButtonEvent& evt) { return mListenerChain.mousePressed(evt); }
-    bool mouseReleased(const MouseButtonEvent& evt) { return mListenerChain.mouseReleased(evt); }
-    bool textInput (const TextInputEvent& evt) { return mListenerChain.textInput (evt); }
-
     void setupContent(void)
     {
-        auto imguiOverlay = new ImGuiOverlay();
-        imguiOverlay->setZOrder(300);
-        imguiOverlay->show();
-        OverlayManager::getSingleton().addOverlay(imguiOverlay); // now owned by overlaymgr
-
-        /*
-            NOTE:
-            Custom apps will ASSERT on ImGuiOverlay::NewFrame() and not display any UI if they
-            have not registered the overlay system by calling mSceneMgr->addRenderQueueListener(mOverlaySystem).
-            OgreBites::SampleBrowser does this on behalf of the ImGuiDemo but custom applications will need to
-            call this themselves.  See ApplicationContextBase::createDummyScene().
-        */
-        mWindow->addListener(this);
-
-        mImguiListener.reset(new ImGuiInputListener());
-        mListenerChain = InputListenerChain({mTrayMgr.get(), mImguiListener.get(), mCameraMan.get()});
 
         mTrayMgr->showCursor();
         mCameraMan->setStyle(OgreBites::CS_ORBIT);
-        mCameraMan->setYawPitchDist(Degree(0), Degree(0), 15);
+        mCameraMan->setYawPitchDist(Degree(0), Degree(0), 600);
 
         SceneNode* lightNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
         lightNode->setPosition(0, 10, 15);
         lightNode->attachObject(mSceneMgr->createLight("MainLight"));
 
-        Entity* ent = mSceneMgr->createEntity("Sinbad.mesh");
-        SceneNode* node = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-        node->attachObject(ent);
-    }
+        auto ps = mSceneMgr->createParticleSystem("ps", "Examples/JetEngine1");
+        auto e = ps->getEmitter(0);
+        e->setMaxTimeToLive(1);
+        e->setMinTimeToLive(1);
+        e->setEmissionRate(500000);
+        ps->setParticleQuota(500000);
 
-    void cleanupContent()
-    {
-        OverlayManager::getSingleton().destroy("ImGuiOverlay");
-        mWindow->removeListener(this);
+        SceneNode* node = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+        node->attachObject(ps);
     }
 };
